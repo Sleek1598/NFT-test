720d09cc6f28df20043d262cc1d4fd02ce398377
Sleek <phillipptrs@gmail.com>
2022-01-30 11:50:26 +0200

    Create gasprice.sh

---
+++ gasprice.sh
@@ -0,0 +1,5 @@
+##Gasprice
+curl https://eth-mainnet.alchemyapi.io/v2/WaNUwOLLaxljBGCXRURqxsEwNVM3ISB7 \
+-X POST \
+-H "Content-Type: application/json" \
+-d '{"jsonrpc":"2.0","method":"eth_gasPrice","params":[],"id":73}'
